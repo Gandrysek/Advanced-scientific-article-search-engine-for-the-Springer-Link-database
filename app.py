8a8da01749b172a05b0551f90829a58cbc3e5f50
Gandrysek <88751200+Gandrysek@users.noreply.github.com>
2024-08-15 02:24:01 +0200

    feat: add results to csv file

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
 import requests
-from flask import Flask, jsonify, request
+import pandas as pd
+from flask import Flask, jsonify, request, send_file
+from io import BytesIO
 
 app = Flask(__name__)
 
-# Springer Link API credentials
+
 SPRINGER_API_KEY = "4907229a03e875715d1f3b3ee17a9df5"
 
-@app.route('/test-springer-query')
-def test_springer_query():
+@app.route('/search', methods=['GET'])
+def search():
+
+    query = request.args.get('query', default='machine learning', type=str)
+    export_csv = request.args.get('export_csv', default='false', type=str).lower() == 'true'
+
 
     base_url = "http://api.springernature.com/meta/v2/json"
 
-    # Testing queries
+    # Query parameters
     params = {
-        'q': 'machine learning',
+        'q': query,
         'api_key': SPRINGER_API_KEY
     }
 
@@ -23,7 +29,27 @@ def test_springer_query():
 
     if response.status_code == 200:
         data = response.json()
-        return jsonify(data['records'][:5])
+        records = data.get('records', [])
+
+        if export_csv:
+
+            df = pd.DataFrame.from_records(records)
+
+
+            csv_output = BytesIO()
+            df.to_csv(csv_output, index=False)
+            csv_output.seek(0)
+
+            # Return the CSV file
+            return send_file(
+                csv_output,
+                mimetype='text/csv',
+                download_name=f'{query}_results.csv',
+                as_attachment=True
+            )
+
+
+        return jsonify(records)
     else:
         return jsonify({"error": "Failed to retrieve data from Springer Link API"}), response.status_code
 
