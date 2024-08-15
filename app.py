b6f2a2c5c1a1ea7577eda76270a79382f74fa825
Gandrysek <88751200+Gandrysek@users.noreply.github.com>
2024-08-15 02:11:20 +0200

    feat: add db mysql and functionality for test queries

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
-from flask import Flask
-from routes import search_blueprint
-from config import Config
+import requests
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
-app.config.from_object(Config)
 
-# Register the blueprint for the routes
-app.register_blueprint(search_blueprint)
+# Springer Link API credentials
+SPRINGER_API_KEY = "4907229a03e875715d1f3b3ee17a9df5"
+
+@app.route('/test-springer-query')
+def test_springer_query():
+
+    base_url = "http://api.springernature.com/meta/v2/json"
+
+    # Testing queries
+    params = {
+        'q': 'machine learning',
+        'api_key': SPRINGER_API_KEY
+    }
+
+
+    response = requests.get(base_url, params=params)
+
+
+    if response.status_code == 200:
+        data = response.json()
+        return jsonify(data['records'][:5])
+    else:
+        return jsonify({"error": "Failed to retrieve data from Springer Link API"}), response.status_code
 
 if __name__ == '__main__':
     app.run(debug=True)
--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 
 class Config:
-    SPRINGER_API_KEY = os.environ.get('SPRINGER_API_KEY', 'KEY')   # key, idk, i wrote them on email from my student mail
-    JSON_SORT_KEYS = False
-    DEBUG = True
+    SPRINGER_API_KEY = os.environ.get('SPRINGER_API_KEY',
+'4907229a03e875715d1f3b3ee17a9df5')  # OUR KEY
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlclient://springer_user:1488@localhost/springer_db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    pass
